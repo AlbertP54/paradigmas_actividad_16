be8470cca6429e186e695491b20394946e3b6fce
Alberto Payan <a311047@uach.mx>
2021-05-13 17:00:36 -0700

    Actividad 16

---
+++ script.js
@@ -0,0 +1,44 @@
+const app = Vue.createApp({
+    data() {
+        return { 
+            materia: "Paradigmas de programacion",
+            semestre: "Sexto factorial",
+            carrera: "Sistemas",
+            productos: [
+                {nombre: "cerveza", cantidad: 100},
+                {nombre: "agua", cantidad: 50},
+                {nombre: "vino", cantidad: 10},
+            ],
+            producto: ""
+        }
+    },
+
+    methods: {
+        agregarProducto() {
+            
+            this.productos.push(
+                {nombre: this.producto, cantidad: 0}
+            )
+            
+            this.producto =""
+        }
+    },
+
+    computed: {
+        voltearTexto() {
+            return this.producto.split("").reverse().join()
+        },
+
+        total() {
+            // let total = 0
+            // for(producto of this.productos){
+            //    total = total + producto.cantidad
+            // }
+            // return total
+            return this.productos.reduce((contador, product) => contador + product.cantidad, 0)
+        }
+
+    }
+})
+
+const vm = app.mount('#app')
